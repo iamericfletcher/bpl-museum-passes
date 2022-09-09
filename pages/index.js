813d8f71110f63ead4f95ac68f53479213c1e186
Yofou <exodias1965@gmail.com>
2022-09-09 21:08:59 +0100

    feat: Migrate all prisma & api calls to backend

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 import * as React from "react";
 import Form from "../src/components/form/Form";
 import Container from "@mui/material/Container";
-// import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
-// import Button from "@mui/material/Button";
-// import BuyMeACoffeeButton from "../src/components/BuyMeACoffeeButton";
-// import InstructionsButton from "../src/components/InstructionsButton";
 
 export default function Index(props) {
     return (
@@ -29,6 +25,10 @@ export async function getServerSideProps() {
 	const res = await fetch("http://localhost:3001")
 		.then(res => res.json())
 		.catch(res => res)
+
+	if (res.message) {
+		// redirect to error page here.
+	}
 
 	return {
 		props: res
