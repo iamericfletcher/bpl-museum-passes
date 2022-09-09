d5836be3536e7b220edd7322226a23a2c5264389
Yofou <exodias1965@gmail.com>
2022-09-09 20:44:23 +0100

    feat: Initial mockup for migration

--- pages/api/requests.js
+++ pages/api/requests.js
@@ -1,44 +1,25 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import { PrismaClient } from '@prisma/client'
-
-// const prisma =
-//
-const prisma = new PrismaClient();
-
 export default async (req, res) => {
-    // Google Recaptcha check
-    const human = validateHuman(req.body.token);
-
-    async function validateHuman(token) {
-        const secret = process.env.RECAPTCHA_SECRET_KEY;
-        const response = await fetch(
-            `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
-            {
-                method: "POST",
-            }
-        );
-        const data = await response.json();
-
-        // return false;
-        return data.success;
-    }
-    // Do this if not passing recaptcha check - validateHuman()
-    if (!human) {
-        res.status(400).json({error: "Unauthorized - failed recaptcha"});
-        return;
-    }
-
     if (req.method !== "POST") {
         return res.status(405).json({
             message: "Method not allowed"
         });
     }
-    // const requestData = JSON.parse(req.body);
-    const requestData = req.body.body;
 
+	console.log(req.body)
 
-    const savedRequest = await prisma.request.create({
-        data: requestData
-    })
-    res.json(savedRequest);
+	const response = await fetch('http://localhost:3001/', {
+		method: "POST",
+		headers: {
+			'Content-type': 'application/json'
+		},
+		body: JSON.stringify(req.body)
+	})
+
+	if (response.status === 400) {
+        res.status(400).json({error: "Unauthorized - failed recaptcha"});
+        return;
+	}
+
+	const data = await response.json()
+	res.json(data)
 }
